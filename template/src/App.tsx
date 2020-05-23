009444a47c85af68db8cbb52758c127ec880efba
Anthony Poschen <zanven42@gmail.com>
2020-05-24 00:27:49 +1000

    updated base template

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,61 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import CSS from 'csstype';
-import { motion } from "framer-motion"
 import { useMediaQuery } from 'react-responsive'
+import styled,{keyframes} from 'styled-components';
 
-const styles = {
-    AppStyle: {
-        textAlign: "center",
-    } as CSS.Properties,
-
-    AppLogoStyle: {
-        height: "40vmin",
-        pointerEvents: "none",
-    } as CSS.Properties,
-
-    AppHeaderStyle: {
-        backgroundColor: "#282c34",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "calc(10px + 2vmin)",
-        color: "white",
-    } as CSS.Properties,
-
-    appLinkStyle: {
-        color: "#61dafb",
-    } as CSS.Properties
+const spin = keyframes`
+from {
+  transform: rotate(0deg);
 }
+to {
+  transform: rotate(360deg);
+}
+`
 
+interface AppProps {
+  Mobile: boolean
+}
+const App = styled.div<AppProps>`
+  display: grid;
+  text-align: center;
+  min-height: 100vh;
+  grid-template-areas: 
+    ". header ." 
+    ". content .";
+  grid-template-rows: ${props => props.Mobile ? "5vh" : "10vh"} auto;
+  grid-template-columns: ${props => props.Mobile ? "0px auto 0px" : "auto 1200px auto"}; 
+`
+const Header = styled.div`
+  grid-area: header;
+  background: red;
 
-function App() {
-  let logoAnimation:any = {};
-  if (useMediaQuery({query: '(prefers-reduced-motion: no-preference)'})) {
-    logoAnimation = {rotate: 360};
+  p {
+    animation: ${spin} infinite 2s linear;
   }
+`
+
+const Content = styled.div`
+  grid-area: content;
+  background: blue;
+`
+// const app = styled.var
+function AppComponent() {
+  const isMobile:boolean = useMediaQuery({ maxWidth: 1199 })
   return (
-    <div className="App" style={styles.AppStyle}>
-      <header className="App-header" style={styles.AppHeaderStyle}>
-        <motion.div animate={logoAnimation} transition={{ duration: 20,loop: Infinity,ease:"linear"}}>
-          <img src={logo} className="App-logo" alt="logo" style={styles.AppLogoStyle} />
-        </motion.div>
+    <App Mobile={isMobile}>
+      <Header>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          header 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={styles.appLinkStyle}
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Header>
+      <Content>
+       content 
+      </Content>
+    </App>
   );
 }
-export default App;
+
+
+export default AppComponent;
